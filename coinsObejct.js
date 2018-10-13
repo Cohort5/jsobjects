cc6cd5508911d61ccb9a27fcc8cd55769db519d7
Paul Costa <paulcosta.student@careerdevs.com>
2018-10-13 14:21:42 +0000

    object-coins

---
+++ coinsObejct.js
@@ -0,0 +1,20 @@
+var assignment = "create and object for coins";
+//variable
+    
+var currencyAbbrev = ["AUD","GBP","EUR","JPY","CHF","USD","AFN","ALL","DZD","AOA"]
+//array ["",]
+
+var usd = {
+    
+    penny:  .01,
+    nickel: .05,
+    quater: .25,
+    halfdollar: .50,
+    
+};
+//object keyName: value, "word"
+// run code, enter "node" into terminal
+
+console.log("Six"+ currencyAbbrev[5]);
+
+
