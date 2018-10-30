1cbffceb17d6a7d05a150df3276acf475d3d90d1
Paul Costa <paulcosta.student@careerdevs.com>
2018-10-30 21:46:56 +0000

    hotelrooms

---
+++ carLoops.js
@@ -0,0 +1,18 @@
+//var teams = ["Patriots", "Cheifs" , "Browns", "Eagles" , "Dolphin"]
+
+// console.log(teams[z])
+
+// for (i=0; i < teams.length; i++) {
+//... console.log(teams[i]); 
+//... }
+
+var cars = ["Porsche" , "Ferrari" , "Lamborginhi" , "Alpha Romeo" , "Jaguar"]
+
+    var x = cars[1];
+    
+    
+    for (i=0; i < cars.length; i++) {
+    
+    console.log(cars[i]);
+    
+    }
---
+++ namesloop.js
@@ -0,0 +1,55 @@
+var cohort5Names = ["Paul" , "Dan" , " Brett" , "Josh" , "Tom" , "Christian" , "Joe" , "Mitch" , "Melissa" , "Nashan" , "Alejandra"];
+// create a four loop that goes through each array that says"Hello Name of person in that array"
+// practice four loops in freecode camp for HW per CAM.
+
+
+for (var i = 0 ; i < cohort5Names.length; i++) {
+//     cohort5Names += "Hello"  ;
+    console.log("Hello", cohort5Names[i]);
+	// console.log(`Hello ${cohort5Names[i]}.`)
+}
+
+> var cohort5Names = ["Paul" , "Dan" , " Brett" , "Josh" , "Tom" , "Christian" , "Joe" , "Mitch" , "Melissa" , "Nashan" , "Alejandra"];
+// create a four loop that goes through each array that says"Hello Name of person in that array"
+// practice four loops in freecode camp for HW per CAM.
+
+var i;
+for (i=0; i < cohort5Names.length; i++) {
+    Hello += cohort5Names[i] + "var";
+}
+
+node
+var cohort5Names = ["Paul" , "Dan" , " Brett" , "Josh" , "Tom" , "Christian" , "Joe" , "Mitch" , "Melissa" , "Nashan" , "Alejandra"];
+undefined
+> cohort5Names
+[ 'Paul',
+  'Dan',
+  ' Brett',
+  'Josh',
+  'Tom',
+  'Christian',
+  'Joe',
+  'Mitch',
+  'Melissa',
+  'Nashan',
+  'Alejandra' ]
+> cohort5Names[cohort5Names.length -1];
+'Alejandra'
+> cohort5Names.push("Bubba");
+> var cohort5Names = ["Paul" , "Dan" , " Brett" , "Josh" , "Tom" , "Christian" , "Joe" , "Mitch" , "Melissa" , "Nashan" , "Alejandra"];
+//control c, twice
+
+var cohort5Names = ["Paul" , "Dan" , " Brett" , "Josh" , "Tom" , "Christian" , "Joe" , "Mitch" , "Melissa" , "Nashan" , "Alejandra"];
+
+// create a four loop that goes through each array that says"Hello Name of person in that array"
+// practice four loops in freecode camp for HW per CAM.
+
+
+for (var i = 0 ; i < cohort5Names.length; i++) {
+//     cohort5Names += "Hello"  ;
+// 	console.log("Hello", cohort5Names[i]);
+	console.log(`Hello ${cohort5Names[i]}.`)
+}
+
+
+
